1cc7527ba628917f12a19dc9c551694fb7bbcca2
Daminin <nicksalcedo717@gmail.com>
2026-07-07 23:51:47 -0500

    fix: update CORS origin URL by removing trailing slash

--- src/pactus_backend/shared/config.py
+++ src/pactus_backend/shared/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
         "http://localhost:8000",
         "http://localhost:3000",
         "http://localhost:9002",
-        "https://pactus-frontend.vercel.app/",
+        "https://pactus-frontend.vercel.app",
         "http://127.0.0.1:3000",
     ]
     DEBUG: bool = Field(default=False)
